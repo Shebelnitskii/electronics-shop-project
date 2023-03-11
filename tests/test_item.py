d447a367eb695dddd27cb851deba911746058289
shebel <Shebelnitskiy@gmail.com>
2023-03-11 16:50:07 +0400

    fix item and add test

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+@pytest.fixture()
+def name():
+    return Item(0,0,0)
+
+def test_calculate_total_price(name):
+    assert name.calculate_total_price() == 0.0
+
+def test_apply_discount(name):
+    assert name.apply_discount() == None
