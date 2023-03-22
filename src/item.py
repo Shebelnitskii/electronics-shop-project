45754268576fc254d5110cbef7f5c5efc0b61a87
Shebel <Shebelnitskiy@gmail.com>
2023-03-22 15:14:28 +0400

    add class Phone and fix class Item

--- src/item.py
+++ src/item.py
@@ -26,6 +26,12 @@ class Item:
     def __str__(self):
         return f'{self.__name}'
 
+    def __add__(self, other):
+        if isinstance(other, Item):
+            return self.quantity + other.quantity
+        else:
+            return TypeError('Item')
+
     @property
     def name(self):
         return f'{self.__name}'
---
+++ src/phone.py
@@ -0,0 +1,22 @@
+from src.item import Item
+class Phone(Item):
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
+        super().__init__(name, price, quantity)
+        self.number_of_sim = number_of_sim
+
+    def __repr__(self):
+        return f"Phone('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+    def __add__(self, other):
+        if isinstance(other, Phone):
+            return self.quantity + other.quantity
+        else:
+            return TypeError("Phone")
+
+    # @property
+    # def number_of_sim(self):
+    #     return f"{self.number_of_sim}"
+    # @number_of_sim.setter
+    # def number_of_sim(self, number_of_sim):
+    #     if number_of_sim <= 0:
+    #         raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
