2243a0c5176e92be2c93837f89f71d0d94cb26bb
Shebel <Shebelnitskiy@gmail.com>
2023-03-29 11:46:32 +0400

    Fix KeyBoard and add class Mixin

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,23 +1,25 @@
 from src.item import Item
 
-class KeyBoard(Item):
+class Mixin(Item):
     def __init__(self,name: str, price: float, quantity: int):
         super().__init__(name,price,quantity)
-        self.__language = "EN"
-
-
-    def change_lang(self):
-        if self.__language == "RU":
-            self.__language = "EN"
-        else:
-            self.__language = "RU"
-        return self
-
+        self._language = 'EN'
 
     @property
     def language(self):
-        return self.__language
+        return self._language
     @language.setter
     def language(self, language):
         if language != "EN" or language != "RU":
             raise AttributeError("property 'language' of 'KeyBoard' object has no setter")
+
+    def change_lang(self):
+        if self._language == 'EN':
+            self._language = 'RU'
+        else:
+            self._language = 'EN'
+        return self
+
+class KeyBoard(Mixin):
+    def __init__(self,name: str, price: float, quantity: int):
+        super().__init__(name,price,quantity)
